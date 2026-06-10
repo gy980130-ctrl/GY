5109d93bdbaf1fe522d43758addaa76ff90b460c
gy980130-ctrl <gy980130@gmail.com>
2026-06-10 16:51:08 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+st.write('안녕하세요 리쿠 여친입니다.')
+
+st.markdown('''
+# 이 글은 마크다운으로 작성됨
+            1. 항목
+            ** 중요한 내용**
+            ''')
+
+st.tabs(["메뉴1", "메뉴2", "메뉴3"])
+tab1, tab2, tab3 = st.tabs(["메뉴1", "메뉴2", "메뉴3"])
+
+
+with tab1:
+    st.write("첫 번째 탭입니다.")
+    with st.expander("자세히보기"):
+        st.write('숨겨진 내용')
+        
+with tab2:
+    st.write("두 번째 탭입니다.")
+    a = st.selectbox("기능", ["복사", "붙여넣기", "자르기"])
+    st.write("선택한 기능:", a)
+
+with tab3:
+    st.write("세 번째 탭입니다.")
+if st.button('버튼누르기'):
+    st.write('버튼이 눌렸습니다.')
